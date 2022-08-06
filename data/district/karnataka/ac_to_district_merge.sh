aec75cdb13e44fa283eadf3db6586d9fd957b994
Vonter <25414711+Vonter@users.noreply.github.com>
2022-08-06 22:29:09 +0530

    Add additional scripts used to merge AC level data

---
+++ data/district/karnataka/ac_to_district_merge.sh
@@ -0,0 +1,32 @@
+geojson-merge ac_{001..018}.geojson > Belagavi.geojson
+geojson-merge ac_{019..025}.geojson > Bagalkot.geojson
+geojson-merge ac_{034..035}.geojson > Kalaburagi.geojson
+geojson-merge ac_{036..039}.geojson > Yadgir.geojson
+geojson-merge ac_{034..035}.geojson ac_{040..046}.geojson > Kalaburagi.geojson
+geojson-merge ac_{047..052}.geojson > Bidar.geojson
+geojson-merge ac_{053..059}.geojson > Raichur.geojson
+geojson-merge ac_{060..064}.geojson > Koppal.geojson
+geojson-merge ac_{065..068}.geojson > Gadag.geojson
+geojson-merge ac_{069..075}.geojson > Dharwad.geojson
+geojson-merge ac_{076..081}.geojson > 'Uttara Kannada.geojson'
+geojson-merge ac_{082..087}.geojson > 'Haveri.geojson'
+geojson-merge ac_{091..095}.geojson > 'Ballary.geojson'
+geojson-merge ac_{097..102}.geojson > 'Chitradurga.geojson'
+geojson-merge ac_{105..110}.geojson ac_103.geojson > 'Davangere.geojson'
+geojson-merge ac_{026..033}.geojson > Vijayapura.geojson
+geojson-merge ac_{088..090}.geojson ac_096.geojson ac_104.geojson > 'Vijaynagara.geojson'
+geojson-merge ac_{111..117}.geojson > Shimoga.geojson
+geojson-merge ac_{118..122}.geojson > Udupi.geojson
+geojson-merge ac_{123..127}.geojson > Chikmagalur.geojson
+geojson-merge ac_{128..138}.geojson > Tumakuru.geojson
+geojson-merge ac_{139..143}.geojson > Chikkaballapura.geojson
+geojson-merge ac_{144..149}.geojson > Kolar.geojson
+geojson-merge ac_{150..177}.geojson > 'Bangalore Urban.geojson'
+geojson-merge ac_{178..181}.geojson > 'Bangalore Rural.geojson'
+geojson-merge ac_{182..185}.geojson > 'Ramanagara.geojson'
+geojson-merge ac_{186..192}.geojson > 'Mandya.geojson'
+geojson-merge ac_{193..199}.geojson > 'Hassan.geojson'
+geojson-merge ac_{200..207}.geojson > 'Dakshina Kannada.geojson'
+geojson-merge ac_{208..209}.geojson > 'Kodagu.geojson'
+geojson-merge ac_{210..220}.geojson > 'Mysore.geojson'
+geojson-merge ac_{221..224}.geojson > 'Chamarajanagar.geojson'
